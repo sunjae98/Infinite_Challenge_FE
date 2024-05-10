fe85b084a293d2add20c82877e516630a1212a4f
kimsunjae <woal9844@gmail.com>
2024-05-10 20:38:18 +0900

    feat : placeholder 수정

--- src/app/_component/SearchBar.tsx
+++ src/app/_component/SearchBar.tsx
@@ -1,15 +1,62 @@
+'use client'
+
 import Image from 'next/image'
+import { useState } from 'react'
+import { SyntheticEvent } from 'react'
 
 export default function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [isInputFocused, setIsInputFocused] = useState(false)
+
+  const handleSearch = () => {
+    console.log('검색어:', searchTerm)
+  }
+
+  const handleInputChange = (e: SyntheticEvent) => {
+    const target = e.target as HTMLInputElement
+    setSearchTerm(target.value)
+  }
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSearch()
+    }
+  }
+
+  const handleFocus = () => {
+    setIsInputFocused(true)
+  }
+
+  const handleBlur = () => {
+    if (searchTerm === '') {
+      setIsInputFocused(false)
+    }
+  }
+
   return (
     <div className="w-[486px] pr-2 bg-white rounded-[42px] justify-between items-center inline-flex">
       <div className="w-[430px] h-[69.70px] pl-6 pr-2.5 py-5 bg-white justify-start items-center gap-2.5 flex rounded-[42px]">
-        <div className="w-4 h-4 relative">
-          <Image src="/search.svg" alt="search" layout="fill" objectFit="contain" priority />
+        <div className={`w-full h-full flex items-center ${isInputFocused ? 'hidden' : ''}`}>
+          <img
+            src="/search.svg"
+            alt="search"
+            className={`w-4 h-4 relative ${isInputFocused ? 'hidden' : ''}`}
+          />
+          <img
+            src="placeholder.svg"
+            alt="placeholder"
+            className={`w-[151px] h-full relative ml-3 ${isInputFocused ? 'hidden' : ''}`}
+          />
         </div>
-        <div className="w-[151px] text-center text-zinc-400 text-base font-normal font-['Spoqa Han Sans Neo'] leading-none">
-          질환명을 입력해주세요.
-        </div>
+        <input
+          type="text"
+          className="w-full h-full outline-none"
+          value={searchTerm}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyPress}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
       </div>
       <div className="w-12 h-12 relative">
         <Image
